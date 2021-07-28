9065e1a1152440b6b566e4ce64448b3f8e486fec
Leandro <leabertero@hotmail.com>
2021-07-27 22:58:37 -0300

    Segundo programa

---
+++ Prueba1/Prueba1/FrmPrueba.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace Prueba1
+{
+    partial class FrmPrueba
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSaludo = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnAceptar = new System.Windows.Forms.Button();
+            this.BtnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSaludo
+            // 
+            this.lblSaludo.AutoSize = true;
+            this.lblSaludo.Font = new System.Drawing.Font("Kristen ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaludo.Location = new System.Drawing.Point(12, 9);
+            this.lblSaludo.Name = "lblSaludo";
+            this.lblSaludo.Size = new System.Drawing.Size(401, 27);
+            this.lblSaludo.TabIndex = 0;
+            this.lblSaludo.Text = "Inicie el programa, para ver el saludo..";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // BtnAceptar
+            // 
+            this.BtnAceptar.BackColor = System.Drawing.Color.Black;
+            this.BtnAceptar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnAceptar.ForeColor = System.Drawing.Color.Red;
+            this.BtnAceptar.Location = new System.Drawing.Point(47, 62);
+            this.BtnAceptar.Name = "BtnAceptar";
+            this.BtnAceptar.Size = new System.Drawing.Size(94, 37);
+            this.BtnAceptar.TabIndex = 2;
+            this.BtnAceptar.Text = "Aceptar";
+            this.BtnAceptar.UseVisualStyleBackColor = false;
+            this.BtnAceptar.Click += new System.EventHandler(this.BtnAceptar_Click);
+            // 
+            // BtnSalir
+            // 
+            this.BtnSalir.BackColor = System.Drawing.Color.Black;
+            this.BtnSalir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnSalir.ForeColor = System.Drawing.Color.Red;
+            this.BtnSalir.Location = new System.Drawing.Point(1195, 230);
+            this.BtnSalir.Name = "BtnSalir";
+            this.BtnSalir.Size = new System.Drawing.Size(64, 26);
+            this.BtnSalir.TabIndex = 3;
+            this.BtnSalir.Text = "Salir";
+            this.BtnSalir.UseVisualStyleBackColor = false;
+            this.BtnSalir.Click += new System.EventHandler(this.BtnSalir_Click);
+            // 
+            // FrmPrueba
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.YellowGreen;
+            this.ClientSize = new System.Drawing.Size(1271, 268);
+            this.Controls.Add(this.BtnSalir);
+            this.Controls.Add(this.BtnAceptar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblSaludo);
+            this.Name = "FrmPrueba";
+            this.Text = "Programa de prrueba (Saludo) - Segundo programa";
+            this.Load += new System.EventHandler(this.FrmPrueba_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSaludo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnAceptar;
+        private System.Windows.Forms.Button BtnSalir;
+    }
+}
+
---
+++ Prueba1/Prueba1/FrmPrueba.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prueba1
+{
+    public partial class FrmPrueba : Form
+    {
+        string primerVariable = "Leandro, ";
+
+        public FrmPrueba()
+        {
+            string SegundaVariable = " como te va? - HACÉ UN CLICK EN EL BOTON (ACEPTAR)";
+            InitializeComponent();
+            lblSaludo.Text = "Hola " + primerVariable + SegundaVariable; 
+        }
+
+        private void FrmPrueba_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnAceptar_Click(object sender, EventArgs e)
+        {
+            string tercerVariable = "HA PRESIONADO EL BOTON ACEPTAR, Y ESTO HA OCASIONADO UN UN ERROR...";
+
+            lblSaludo.Text = "Hola " + primerVariable + tercerVariable;
+        }
+
+        private void BtnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
