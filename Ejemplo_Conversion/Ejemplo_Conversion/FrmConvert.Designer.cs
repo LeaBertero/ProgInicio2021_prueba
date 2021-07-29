b7015885ce749806cb12c9cba90c696ef497b21d
Leandro <leabertero@hotmail.com>
2021-07-29 20:16:54 -0300

    Se ha retocado nuevamente el diseño del formulario

--- Ejemplo_Conversion/Ejemplo_Conversion/FrmConvert.Designer.cs
+++ Ejemplo_Conversion/Ejemplo_Conversion/FrmConvert.Designer.cs
@@ -39,6 +39,7 @@ namespace Ejemplo_Conversion
             this.label4 = new System.Windows.Forms.Label();
             this.LblRes = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // TxtSum2
@@ -46,7 +47,7 @@ namespace Ejemplo_Conversion
             this.TxtSum2.BackColor = System.Drawing.Color.Black;
             this.TxtSum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TxtSum2.ForeColor = System.Drawing.Color.Red;
-            this.TxtSum2.Location = new System.Drawing.Point(436, 162);
+            this.TxtSum2.Location = new System.Drawing.Point(262, 189);
             this.TxtSum2.Name = "TxtSum2";
             this.TxtSum2.Size = new System.Drawing.Size(100, 26);
             this.TxtSum2.TabIndex = 0;
@@ -56,7 +57,7 @@ namespace Ejemplo_Conversion
             this.TxtSum1.BackColor = System.Drawing.Color.Black;
             this.TxtSum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TxtSum1.ForeColor = System.Drawing.Color.Red;
-            this.TxtSum1.Location = new System.Drawing.Point(207, 162);
+            this.TxtSum1.Location = new System.Drawing.Point(33, 189);
             this.TxtSum1.Name = "TxtSum1";
             this.TxtSum1.Size = new System.Drawing.Size(100, 26);
             this.TxtSum1.TabIndex = 1;
@@ -67,7 +68,7 @@ namespace Ejemplo_Conversion
             this.BtnRes.Cursor = System.Windows.Forms.Cursors.Hand;
             this.BtnRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnRes.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.BtnRes.Location = new System.Drawing.Point(603, 159);
+            this.BtnRes.Location = new System.Drawing.Point(429, 186);
             this.BtnRes.Name = "BtnRes";
             this.BtnRes.Size = new System.Drawing.Size(80, 34);
             this.BtnRes.TabIndex = 2;
@@ -79,7 +80,7 @@ namespace Ejemplo_Conversion
             // 
             this.Label6.AutoSize = true;
             this.Label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Label6.Location = new System.Drawing.Point(355, 167);
+            this.Label6.Location = new System.Drawing.Point(181, 194);
             this.Label6.Name = "Label6";
             this.Label6.Size = new System.Drawing.Size(19, 20);
             this.Label6.TabIndex = 3;
@@ -89,7 +90,7 @@ namespace Ejemplo_Conversion
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(191, 116);
+            this.label1.Location = new System.Drawing.Point(17, 143);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(125, 20);
             this.label1.TabIndex = 4;
@@ -99,7 +100,7 @@ namespace Ejemplo_Conversion
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(409, 118);
+            this.label2.Location = new System.Drawing.Point(235, 145);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(146, 20);
             this.label2.TabIndex = 5;
@@ -109,7 +110,7 @@ namespace Ejemplo_Conversion
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(615, 118);
+            this.label3.Location = new System.Drawing.Point(441, 145);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(49, 20);
             this.label3.TabIndex = 6;
@@ -119,7 +120,7 @@ namespace Ejemplo_Conversion
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(720, 118);
+            this.label4.Location = new System.Drawing.Point(546, 145);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(91, 20);
             this.label4.TabIndex = 7;
@@ -129,7 +130,7 @@ namespace Ejemplo_Conversion
             // 
             this.LblRes.AutoSize = true;
             this.LblRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblRes.Location = new System.Drawing.Point(735, 164);
+            this.LblRes.Location = new System.Drawing.Point(561, 191);
             this.LblRes.Name = "LblRes";
             this.LblRes.Size = new System.Drawing.Size(0, 20);
             this.LblRes.TabIndex = 8;
@@ -138,21 +139,30 @@ namespace Ejemplo_Conversion
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Kristen ITC", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(13, 41);
+            this.label5.Location = new System.Drawing.Point(25, 19);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(1029, 29);
+            this.label5.Size = new System.Drawing.Size(374, 29);
             this.label5.TabIndex = 9;
-            this.label5.Text = "Ingrese dos números para sumar, en caso de ingresar un decimal, derá hacerlo con " +
-    "coma...";
+            this.label5.Text = "Ingrese dos números para sumar";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Kristen ITC", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(16, 90);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(511, 29);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Indique los números ingresados, con coma...";
             // 
             // FrmConvert
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Orange;
-            this.ClientSize = new System.Drawing.Size(1039, 275);
+            this.ClientSize = new System.Drawing.Size(639, 239);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.LblRes);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -161,6 +171,7 @@ namespace Ejemplo_Conversion
             this.Controls.Add(this.BtnRes);
             this.Controls.Add(this.TxtSum1);
             this.Controls.Add(this.TxtSum2);
+            this.Controls.Add(this.LblRes);
             this.Name = "FrmConvert";
             this.Text = "Ejemplo_Conversion";
             this.Load += new System.EventHandler(this.FrmConvert_Load);
@@ -181,6 +192,7 @@ namespace Ejemplo_Conversion
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label LblRes;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
     }
 }
 
