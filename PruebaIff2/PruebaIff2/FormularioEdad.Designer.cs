51151b77f8fd0251c31fc29dc2d490dce2f1b50a
Leandro <leabertero@hotmail.com>
2021-08-02 18:11:32 -0300

    nuevos cambios

--- PruebaIff2/PruebaIff2/FormularioEdad.Designer.cs
+++ PruebaIff2/PruebaIff2/FormularioEdad.Designer.cs
@@ -51,13 +51,16 @@ namespace PruebaIff2
             // 
             // BtnAceptar
             // 
+            this.BtnAceptar.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.BtnAceptar.Cursor = System.Windows.Forms.Cursors.Hand;
             this.BtnAceptar.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAceptar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.BtnAceptar.Location = new System.Drawing.Point(130, 335);
             this.BtnAceptar.Name = "BtnAceptar";
             this.BtnAceptar.Size = new System.Drawing.Size(91, 30);
             this.BtnAceptar.TabIndex = 1;
             this.BtnAceptar.Text = "Aceptar";
-            this.BtnAceptar.UseVisualStyleBackColor = true;
+            this.BtnAceptar.UseVisualStyleBackColor = false;
             this.BtnAceptar.Click += new System.EventHandler(this.BtnAceptar_Click);
             // 
             // TxtEdad
@@ -124,7 +127,7 @@ namespace PruebaIff2
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.OliveDrab;
-            this.ClientSize = new System.Drawing.Size(333, 481);
+            this.ClientSize = new System.Drawing.Size(350, 481);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
