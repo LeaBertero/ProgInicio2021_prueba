6d23b7b55e4191f6ccbe0e290bcf8b0434d03c44
Leandro <leabertero@hotmail.com>
2021-08-02 02:10:15 -0300

    Programa edad_fin

---
+++ PruebaIff2/PruebaIff2/FormularioEdad.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace PruebaIff2
+{
+    partial class FormularioEdad
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbletiqueta = new System.Windows.Forms.Label();
+            this.BtnAceptar = new System.Windows.Forms.Button();
+            this.TxtEdad = new System.Windows.Forms.TextBox();
+            this.lblRes = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbletiqueta
+            // 
+            this.lbletiqueta.AutoSize = true;
+            this.lbletiqueta.Font = new System.Drawing.Font("Tempus Sans ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbletiqueta.Location = new System.Drawing.Point(33, 277);
+            this.lbletiqueta.Name = "lbletiqueta";
+            this.lbletiqueta.Size = new System.Drawing.Size(153, 24);
+            this.lbletiqueta.TabIndex = 0;
+            this.lbletiqueta.Text = "Ingrese su edad:";
+            // 
+            // BtnAceptar
+            // 
+            this.BtnAceptar.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAceptar.Location = new System.Drawing.Point(130, 335);
+            this.BtnAceptar.Name = "BtnAceptar";
+            this.BtnAceptar.Size = new System.Drawing.Size(91, 30);
+            this.BtnAceptar.TabIndex = 1;
+            this.BtnAceptar.Text = "Aceptar";
+            this.BtnAceptar.UseVisualStyleBackColor = true;
+            this.BtnAceptar.Click += new System.EventHandler(this.BtnAceptar_Click);
+            // 
+            // TxtEdad
+            // 
+            this.TxtEdad.BackColor = System.Drawing.Color.PaleGreen;
+            this.TxtEdad.Font = new System.Drawing.Font("Tempus Sans ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtEdad.ForeColor = System.Drawing.Color.Red;
+            this.TxtEdad.Location = new System.Drawing.Point(33, 333);
+            this.TxtEdad.Name = "TxtEdad";
+            this.TxtEdad.Size = new System.Drawing.Size(60, 32);
+            this.TxtEdad.TabIndex = 2;
+            // 
+            // lblRes
+            // 
+            this.lblRes.AutoSize = true;
+            this.lblRes.Font = new System.Drawing.Font("Tempus Sans ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRes.Location = new System.Drawing.Point(33, 400);
+            this.lblRes.Name = "lblRes";
+            this.lblRes.Size = new System.Drawing.Size(0, 24);
+            this.lblRes.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tempus Sans ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Edad < 12: Niño";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tempus Sans ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(33, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(265, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Edad = 15 y 20: Adolescente";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tempus Sans ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(33, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(188, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Edad = + 21: Adulto";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tempus Sans ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(33, 194);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(257, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Edad = + 50: Adulto Mayor";
+            // 
+            // FormularioEdad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OliveDrab;
+            this.ClientSize = new System.Drawing.Size(333, 481);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblRes);
+            this.Controls.Add(this.TxtEdad);
+            this.Controls.Add(this.BtnAceptar);
+            this.Controls.Add(this.lbletiqueta);
+            this.Name = "FormularioEdad";
+            this.Text = "Prueba If - segunda parte";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbletiqueta;
+        private System.Windows.Forms.Button BtnAceptar;
+        private System.Windows.Forms.TextBox TxtEdad;
+        private System.Windows.Forms.Label lblRes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ PruebaIff2/PruebaIff2/FormularioEdad.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebaIff2
+{
+    public partial class FormularioEdad : Form
+    {
+        public FormularioEdad()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAceptar_Click(object sender, EventArgs e)
+        {
+            int edad = System.Convert.ToInt32(TxtEdad.Text);
+
+            if (edad <= 12)
+            {
+                lblRes.Text = "Esta persona, es un niño";
+            }
+            else if (edad >= 15 && edad < 20)
+            {
+                lblRes.Text = "esta persona, es un adolescente";
+            }
+            if (edad >= 21)
+            {
+                lblRes.Text = "Esta persona, es un adulto";
+            }
+            if (edad >= 50)
+            {
+                lblRes.Text = "Esta persona es un adulto mayor";
+            }
+        }
+    }
+}
