1028aa6342e4f0ef25c2a1fe7e26cb928ac7e9e1
Leandro <leabertero@hotmail.com>
2021-08-02 22:18:50 -0300

    Ejemplo Switch

---
+++ Ejemplo Swich (Según)/Ejemplo Swich/Switch_arranque.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace Ejemplo_Swich
+{
+    partial class Switch_arranque
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnAceptar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LblRes = new System.Windows.Forms.Label();
+            this.TxtEdad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnAceptar
+            // 
+            this.BtnAceptar.BackColor = System.Drawing.Color.Black;
+            this.BtnAceptar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnAceptar.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAceptar.ForeColor = System.Drawing.Color.Gold;
+            this.BtnAceptar.Location = new System.Drawing.Point(445, 79);
+            this.BtnAceptar.Name = "BtnAceptar";
+            this.BtnAceptar.Size = new System.Drawing.Size(107, 35);
+            this.BtnAceptar.TabIndex = 0;
+            this.BtnAceptar.Text = "Aceptar";
+            this.BtnAceptar.UseVisualStyleBackColor = false;
+            this.BtnAceptar.Click += new System.EventHandler(this.BtnAceptar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tempus Sans ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(312, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Indique su edad..";
+            // 
+            // LblRes
+            // 
+            this.LblRes.AutoSize = true;
+            this.LblRes.Font = new System.Drawing.Font("Tempus Sans ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblRes.Location = new System.Drawing.Point(312, 143);
+            this.LblRes.Name = "LblRes";
+            this.LblRes.Size = new System.Drawing.Size(0, 24);
+            this.LblRes.TabIndex = 2;
+            // 
+            // TxtEdad
+            // 
+            this.TxtEdad.BackColor = System.Drawing.Color.Khaki;
+            this.TxtEdad.Font = new System.Drawing.Font("Tempus Sans ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtEdad.ForeColor = System.Drawing.Color.Red;
+            this.TxtEdad.Location = new System.Drawing.Point(316, 79);
+            this.TxtEdad.Name = "TxtEdad";
+            this.TxtEdad.Size = new System.Drawing.Size(88, 32);
+            this.TxtEdad.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tempus Sans ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(243, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Edad = 10: Menor de edad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tempus Sans ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(239, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Edad = 21: Mayor de edad";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tempus Sans ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 188);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(177, 24);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Edad = 40: Adulto";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tempus Sans ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(13, 258);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(239, 24);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Edad = 60: Adulto Mayor";
+            // 
+            // Switch_arranque
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOrange;
+            this.ClientSize = new System.Drawing.Size(580, 354);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtEdad);
+            this.Controls.Add(this.LblRes);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnAceptar);
+            this.Name = "Switch_arranque";
+            this.Text = "Ejemplo Switch (Según)";
+            this.Load += new System.EventHandler(this.Swuich_arranque_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnAceptar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LblRes;
+        private System.Windows.Forms.TextBox TxtEdad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Ejemplo Swich (Según)/Ejemplo Swich/Switch_arranque.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejemplo_Swich
+{
+    public partial class Switch_arranque : Form
+    {
+        public Switch_arranque()
+        {
+            InitializeComponent();
+        }
+
+        private void Swuich_arranque_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnAceptar_Click(object sender, EventArgs e)
+        {
+            int edad = System.Convert.ToInt32 (TxtEdad.Text);
+
+            switch (edad)
+            {
+                case 10:
+                    LblRes.Text = "Es Menor de edad";
+                    break;
+                case 21:
+                LblRes.Text = "Es Mayor de edad";
+                    break;
+                case 40:
+                    LblRes.Text = "Es un adulto";
+                    break;
+                case 60:
+                    LblRes.Text = "Es un adulto Mayor";
+                    break;
+
+            }
+        }
+    }
+}
