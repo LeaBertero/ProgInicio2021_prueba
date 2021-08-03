d48307d894e63299a700c83dbb1e5f1ad99a3d07
Leandro <leabertero@hotmail.com>
2021-08-02 22:53:26 -0300

    Ejemplo Switch_parte 2_notas

---
+++ Ejemplo Switch (Segunda parte)/Ejemplo Switch (Segunda parte)/Switch_arranque.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace Ejemplo_Switch__Segunda_parte_
+{
+    partial class Switch_arranque
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnAceptar = new System.Windows.Forms.Button();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.LblRes = new System.Windows.Forms.Label();
+            this.TxtNota = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnAceptar
+            // 
+            this.BtnAceptar.BackColor = System.Drawing.Color.Black;
+            this.BtnAceptar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnAceptar.Font = new System.Drawing.Font("Tempus Sans ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAceptar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BtnAceptar.Location = new System.Drawing.Point(12, 112);
+            this.BtnAceptar.Name = "BtnAceptar";
+            this.BtnAceptar.Size = new System.Drawing.Size(123, 39);
+            this.BtnAceptar.TabIndex = 0;
+            this.BtnAceptar.Text = "Aceptar";
+            this.BtnAceptar.UseVisualStyleBackColor = false;
+            this.BtnAceptar.Click += new System.EventHandler(this.BtnAceptar_Click);
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Font = new System.Drawing.Font("Tempus Sans ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label1.Location = new System.Drawing.Point(12, 9);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(149, 24);
+            this.Label1.TabIndex = 1;
+            this.Label1.Text = "Ingrese la nota:";
+            // 
+            // LblRes
+            // 
+            this.LblRes.AutoSize = true;
+            this.LblRes.Font = new System.Drawing.Font("Tempus Sans ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblRes.Location = new System.Drawing.Point(12, 200);
+            this.LblRes.Name = "LblRes";
+            this.LblRes.Size = new System.Drawing.Size(0, 24);
+            this.LblRes.TabIndex = 2;
+            // 
+            // TxtNota
+            // 
+            this.TxtNota.BackColor = System.Drawing.Color.Black;
+            this.TxtNota.Font = new System.Drawing.Font("Tempus Sans ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtNota.ForeColor = System.Drawing.Color.Red;
+            this.TxtNota.Location = new System.Drawing.Point(16, 52);
+            this.TxtNota.Name = "TxtNota";
+            this.TxtNota.Size = new System.Drawing.Size(100, 32);
+            this.TxtNota.TabIndex = 3;
+            // 
+            // Switch_arranque
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(206, 328);
+            this.Controls.Add(this.TxtNota);
+            this.Controls.Add(this.LblRes);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.BtnAceptar);
+            this.Name = "Switch_arranque";
+            this.Text = "Ejemplo Switch (Según) - Segunda parte";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnAceptar;
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Label LblRes;
+        private System.Windows.Forms.TextBox TxtNota;
+    }
+}
+
---
+++ Ejemplo Switch (Segunda parte)/Ejemplo Switch (Segunda parte)/Switch_arranque.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejemplo_Switch__Segunda_parte_
+{
+    public partial class Switch_arranque : Form
+    {
+        public Switch_arranque()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAceptar_Click(object sender, EventArgs e)
+        {
+            int nota = System.Convert.ToInt32(TxtNota.Text);
+            
+            switch (nota)
+            {
+                case 1:
+                    LblRes.Text = "Aplazado";
+                    break;
+                case 2:
+                    LblRes.Text = "Aplazado";
+                    break;
+                case 3:
+                    LblRes.Text = "Aprobado";
+                    break;
+                case 4:
+                    LblRes.Text = "Promocionado";
+                    break;
+                case 5:
+                    LblRes.Text = "Promocionado";
+                    break;
+                default:
+                    LblRes.Text = "Nota no valida";
+                    break;
+            }
+        }
+    }
+}
