0504b2e82b7bcb9ca4c267a2cb69732619e9002f
Leandro <leabertero@hotmail.com>
2021-08-04 02:10:12 -0300

    EjemploWhile_MovimientoLabel

--- Ejemplo_while/Ejemplo_while/EjemploWhile.Designer.cs
+++ Ejemplo_while/Ejemplo_while/EjemploWhile.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Ejemplo_while
 {
-    partial class Form1
+    partial class EjemploWhile
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -29,13 +29,52 @@ namespace Ejemplo_while
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BtnAceptar = new System.Windows.Forms.Button();
+            this.LblMov = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnAceptar
+            // 
+            this.BtnAceptar.BackColor = System.Drawing.Color.Yellow;
+            this.BtnAceptar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnAceptar.Font = new System.Drawing.Font("Tempus Sans ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAceptar.ForeColor = System.Drawing.Color.Teal;
+            this.BtnAceptar.Location = new System.Drawing.Point(238, 12);
+            this.BtnAceptar.Name = "BtnAceptar";
+            this.BtnAceptar.Size = new System.Drawing.Size(92, 33);
+            this.BtnAceptar.TabIndex = 0;
+            this.BtnAceptar.Text = "Aceptar";
+            this.BtnAceptar.UseVisualStyleBackColor = false;
+            this.BtnAceptar.Click += new System.EventHandler(this.BtnAceptar_Click);
+            // 
+            // LblMov
+            // 
+            this.LblMov.BackColor = System.Drawing.Color.Red;
+            this.LblMov.Font = new System.Drawing.Font("Kristen ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblMov.Location = new System.Drawing.Point(0, 66);
+            this.LblMov.Name = "LblMov";
+            this.LblMov.Size = new System.Drawing.Size(70, 76);
+            this.LblMov.TabIndex = 1;
+            // 
+            // EjemploWhile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.GreenYellow;
+            this.ClientSize = new System.Drawing.Size(561, 232);
+            this.Controls.Add(this.LblMov);
+            this.Controls.Add(this.BtnAceptar);
+            this.Name = "EjemploWhile";
+            this.Text = "Ejemplo_while";
+            this.Load += new System.EventHandler(this.EjemploWhile_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnAceptar;
+        private System.Windows.Forms.Label LblMov;
     }
 }
 
--- Ejemplo_while/Ejemplo_while/EjemploWhile.cs
+++ Ejemplo_while/Ejemplo_while/EjemploWhile.cs
@@ -10,11 +10,31 @@ using System.Windows.Forms;
 
 namespace Ejemplo_while
 {
-    public partial class Form1 : Form
+    public partial class EjemploWhile : Form
     {
-        public Form1()
+        public EjemploWhile()
         {
             InitializeComponent();
+        }
+
+        private void BtnAceptar_Click(object sender, EventArgs e)
+        {
+            int AnchoFormulario = 0;
+            AnchoFormulario = this.Width;
+            int AnchoLabel = LblMov.Width;
+            int AnchoBorde = 15;
+
+            
+            for (int X = 0;  X < AnchoFormulario-AnchoLabel-AnchoBorde; X++)
+            {
+                LblMov.Left = X;
+
+            }
+        }
+
+        private void EjemploWhile_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
