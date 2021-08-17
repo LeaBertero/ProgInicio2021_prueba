0701ee66b1a74837a0f6284ed62f485aa44a6e00
Leandro <leabertero@hotmail.com>
2021-08-16 22:27:10 -0300

    prueba 1

---
+++ HolaMundo2/HolaMundo2/HolaAula.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace HolaMundo2
+{
+    partial class HolaAula
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblEtiqueta = new System.Windows.Forms.Label();
+            this.LblMensaje = new System.Windows.Forms.Label();
+            this.BtnInicio = new System.Windows.Forms.Button();
+            this.TxtMensaje = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // LblEtiqueta
+            // 
+            this.LblEtiqueta.AutoSize = true;
+            this.LblEtiqueta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblEtiqueta.Location = new System.Drawing.Point(51, 9);
+            this.LblEtiqueta.Name = "LblEtiqueta";
+            this.LblEtiqueta.Size = new System.Drawing.Size(236, 20);
+            this.LblEtiqueta.TabIndex = 0;
+            this.LblEtiqueta.Text = "Ingrese el nombre de la materia:";
+            // 
+            // LblMensaje
+            // 
+            this.LblMensaje.AutoSize = true;
+            this.LblMensaje.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblMensaje.Location = new System.Drawing.Point(51, 99);
+            this.LblMensaje.Name = "LblMensaje";
+            this.LblMensaje.Size = new System.Drawing.Size(0, 20);
+            this.LblMensaje.TabIndex = 1;
+            // 
+            // BtnInicio
+            // 
+            this.BtnInicio.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnInicio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnInicio.Location = new System.Drawing.Point(442, 183);
+            this.BtnInicio.Name = "BtnInicio";
+            this.BtnInicio.Size = new System.Drawing.Size(112, 34);
+            this.BtnInicio.TabIndex = 2;
+            this.BtnInicio.Text = "Iniciar";
+            this.BtnInicio.UseVisualStyleBackColor = true;
+            this.BtnInicio.Click += new System.EventHandler(this.BtnInicio_Click);
+            // 
+            // TxtMensaje
+            // 
+            this.TxtMensaje.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtMensaje.Location = new System.Drawing.Point(55, 44);
+            this.TxtMensaje.Name = "TxtMensaje";
+            this.TxtMensaje.Size = new System.Drawing.Size(181, 26);
+            this.TxtMensaje.TabIndex = 3;
+            // 
+            // HolaAula
+            // 
+            this.AcceptButton = this.BtnInicio;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(66)))), ((int)(((byte)(170)))));
+            this.ClientSize = new System.Drawing.Size(566, 229);
+            this.Controls.Add(this.TxtMensaje);
+            this.Controls.Add(this.BtnInicio);
+            this.Controls.Add(this.LblMensaje);
+            this.Controls.Add(this.LblEtiqueta);
+            this.Name = "HolaAula";
+            this.Text = "Hola gente del aula";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblEtiqueta;
+        private System.Windows.Forms.Label LblMensaje;
+        private System.Windows.Forms.Button BtnInicio;
+        private System.Windows.Forms.TextBox TxtMensaje;
+    }
+}
+
